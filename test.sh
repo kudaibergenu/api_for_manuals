2fb112ab0b317a0288a245c5bdd23ac08a5baa13
Kudaibergen Urinbayev <kudaibergenu@Kudaibergens-MacBook-Air.local>
2024-04-29 16:21:47 +0200

    Dockerfile added

--- test.sh
+++ test.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Build the Docker image
+docker build -t myflaskapp .
+
+# Run the Docker container
+docker run -d --name myflaskcontainer -p 5000:5000 myflaskapp
+
+# Wait for the container to fully start and the Flask app to be ready
+echo "Waiting for the Flask app to start..."
+sleep 10
+
 # Endpoint of the Flask API
 API_ENDPOINT="http://127.0.0.1:5000/process-pdf"
 
@@ -14,3 +24,7 @@ curl -X POST "$API_ENDPOINT" \
      -H "Content-Type: multipart/form-data" \
      -F "file=@\"$PDF_FILE_PATH\";type=application/pdf" \
      -F "pages=$PAGES"
+
+# Optionally, you could add a command to stop and remove the container after the API call
+docker stop myflaskcontainer
+docker rm myflaskcontainer
